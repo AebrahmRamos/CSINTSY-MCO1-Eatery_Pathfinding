f97b040659851e5804ba215b0f9682e1a6a5dbef
Johan Marlo Cabili <jmtcabili@gmail.com>
2024-10-20 11:05:59 +0800

    stage changes

--- Archived/Node.java
+++ Archived/Node.java
@@ -4,16 +4,20 @@ public class Node {
     double latitude;
     double longitude;
     String id;
-    List<Node> neighbors;
+    boolean isEatery; 
+    Map<Node, Float> neighbors; //neighhbor and their weight
 
     public Node(double latitude, double longitude, String id) {
         this.latitude = latitude;
         this.longitude = longitude;
         this.id = id;
-        this.neighbors = new ArrayList<>();
+        this.neighbors = new HashMap<>();
     }
 
-    public void addNeighbor(Node neighbor) {
-        neighbors.add(neighbor);
+    public void addNeighbor(Node neighbor, Float weight) {
+        neighbors.put(neighbor, weight);
+        System.out.println("Added node between " + this.id + "and " + neighbor.id);
     }
+
+    
 }
